e11570f576d6dabb8a1eb40b28ec635a321bc121
KondwaniChikaonda <bsc-com-10-22@unima.ac.mw>
2024-08-12 09:29:58 +0200

    created a home page, still in progress

---
+++ app/Sidebar.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="bg-black text-white h-screen w-40 p-4">
+      <h2 className="text-xl font-bold mb-4">Capture</h2>
+      <ul>
+        <li className="mb-2"><a href="#" className="hover:underline">Home</a></li>
+    
+
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
 
-export default function Home() {
+import React from 'react';
+import Sidebar from './Sidebar'; 
+
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <h1  className="text-2xl font-semibold" >Hello Fam</h1>
-    </main>
+    <div className="flex">
+      <Sidebar />   {/* I have imported Sidebar component */}
+      <div className="flex-grow p-4">
+        <div className="flex items-center p-2 row">
+          <div className="flex items-center p-2 px-4 row">
+            <div className="bg-black text-white p-2 rounded-xl m-2">
+              Upload
+            </div>
+            <div className="bg-black text-white p-2 rounded-xl">
+              Record
+            </div>
+          </div>
+        </div>
+
+
+        <hr className="border-t-2 border-gray-300 mt-2 mx-4" />
+
+
+               {/* using a url for now*/}
+        <div className="flex items-center">
+          <div className="video-container flex pl-8 mt-4 row">
+            <video
+              src="https://youtu.be/PN6fRSr4ciM?t=14"   
+              controls
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
